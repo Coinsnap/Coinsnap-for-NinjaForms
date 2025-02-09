e48dd5db30e80b3a54ab3edfc88e034a363bb8f8
adagiomedia <adagiomediarus@gmail.com>
2025-02-09 10:50:18 +0100

    SNAP-1328 Added Ninja Forms to all the Coinsnap connection messages

--- coinsnap-for-ninjaforms.php
+++ coinsnap-for-ninjaforms.php
@@ -88,13 +88,13 @@ if ( ! defined( 'ABSPATH' ) ){
             
                if(!isset($coinsnap_store_id) || empty($coinsnap_store_id)){
                     echo '<div class="notice notice-error"><p>';
-                    esc_html_e('Coinsnap Store ID is not set', 'coinsnap-for-ninjaforms');
+                    esc_html_e('Ninja Forms: Coinsnap Store ID is not set', 'coinsnap-for-ninjaforms');
                     echo '</p></div>';
                 }
 
                 if(!isset($coinsnap_api_key) || empty($coinsnap_api_key)){
                     echo '<div class="notice notice-error"><p>';
-                    esc_html_e('Coinsnap API Key is not set', 'coinsnap-for-ninjaforms');
+                    esc_html_e('Ninja Forms: Coinsnap API Key is not set', 'coinsnap-for-ninjaforms');
                     echo '</p></div>';
                 }
                 
@@ -103,7 +103,7 @@ if ( ! defined( 'ABSPATH' ) ){
                     $store = $client->getStore($coinsnap_store_id);
                     if ($store['code'] === 200) {
                         echo '<div class="notice notice-success"><p>';
-                        esc_html_e('Established connection to Coinsnap Server', 'coinsnap-for-ninjaforms');
+                        esc_html_e('Ninja Forms: Established connection to Coinsnap Server', 'coinsnap-for-ninjaforms');
                         echo '</p></div>';
                         
                         $form_id = filter_input(INPUT_GET,'form_id',FILTER_VALIDATE_INT);
@@ -115,18 +115,18 @@ if ( ! defined( 'ABSPATH' ) ){
                             if ( ! $CoinsnapPG->webhookExists( $coinsnap_store_id, $coinsnap_api_key, $coinsnap_webhook_url ) ) {
                                 if ( ! $CoinsnapPG->registerWebhook( $coinsnap_store_id, $coinsnap_api_key, $coinsnap_webhook_url ) ) {
                                     echo '<div class="notice notice-error"><p>';
-                                    esc_html_e('Unable to create webhook on Coinsnap Server', 'coinsnap-for-ninjaforms');
+                                    esc_html_e('Ninja Forms: Unable to create webhook on Coinsnap Server', 'coinsnap-for-ninjaforms');
                                     echo '</p></div>';
                                 }
                                 else {
                                     echo '<div class="notice notice-success"><p>';
-                                    esc_html_e('Successfully registered a new webhook on Coinsnap Server', 'coinsnap-for-ninjaforms');
+                                    esc_html_e('Ninja Forms: Successfully registered webhook on Coinsnap Server', 'coinsnap-for-ninjaforms');
                                     echo '</p></div>';
                                 }
                             }
                             else {
                                 echo '<div class="notice notice-info"><p>';
-                                esc_html_e('Webhook already exists, skipping webhook creation', 'coinsnap-for-ninjaforms');
+                                esc_html_e('Ninja Forms: Webhook already exists, skipping webhook creation', 'coinsnap-for-ninjaforms');
                                 echo '</p></div>';
                             }
                             
@@ -134,7 +134,7 @@ if ( ! defined( 'ABSPATH' ) ){
                     }
                     else {
                         echo '<div class="notice notice-error"><p>';
-                        esc_html_e('Coinsnap connection error:', 'coinsnap-for-ninjaforms');
+                        esc_html_e('Ninja Forms: Coinsnap connection error:', 'coinsnap-for-ninjaforms');
                         echo esc_html($store['result']['message']);
                         echo '</p></div>';
                     }
